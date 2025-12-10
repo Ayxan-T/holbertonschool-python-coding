06d488bcd3d15d9e2770231dac1dfc98c562d378
Me <me@example.com>
2025-12-10 12:25:46 +0000

    auto commit

---
+++ python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    a module with a class Square
+"""
+
+
+class Square:
+    """
+        a class for defining a Square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size*self.__size
+
+    def my_print(self):
+        for i in range(self.size):
+            for i in range(self.size):
+                print("#", end='')
+            print()
